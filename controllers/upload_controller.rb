67acd81585f0df54e8ce3fab4b174d971cb9a5c7
Han Hermanides <han@hermanides.com>
2008-09-04 21:32:24 +0200

    weer een nieuwe versie

--- controllers/upload_controller.rb
+++ controllers/upload_controller.rb
@@ -1,8 +1,16 @@
 class UploadController < ApplicationController
+  in_place_edit_for :picture, :url
 
-def index
-redirect_to :action => 'get'
-end
+  protect_from_forgery :only => [:update, :delete, :create]
+
+  def index
+    redirect_to :action => 'get'
+  end
+
+  def get_picture_url
+    @picture = Picture.find(params[:id])
+    render :text => @picture.url
+  end
 
   def get
     @picture = Picture.new
